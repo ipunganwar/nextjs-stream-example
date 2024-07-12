6910b4cabcfb1c8913a0ca66e3aef9addf006cd6
ipunganwar <ipung.anwar1@gmail.com>
2024-07-12 11:07:14 +0700

    chore: init sidebar

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    imgUrl: "/icons/Home.svg",
+    route: "/",
+  },
+  {
+    label: "Upcoming",
+    imgUrl: "/icons/Upcoming.svg",
+    route: "/upcoming",
+  },
+  {
+    label: "Previous",
+    imgUrl: "/icons/Previous.svg",
+    route: "/previous",
+  },
+  {
+    label: "Recording",
+    imgUrl: "/icons/Video.svg",
+    route: "/recording",
+  },
+  {
+    label: "Personal Room",
+    imgUrl: "/icons/Add-personal.svg",
+    route: "/personal-room",
+  },
+];
