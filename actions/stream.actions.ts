f9f230de50e801bd9655c73fe8419af249ab3df3
ipunganwar <ipung.anwar1@gmail.com>
2024-07-13 10:52:25 +0700

    chore: set custom hooks

--- actions/stream.actions.ts
+++ actions/stream.actions.ts
@@ -3,21 +3,21 @@
 import { currentUser } from "@clerk/nextjs/server";
 import { StreamClient } from "@stream-io/node-sdk";
 
-const apiKey = process.env.NEXT_PUBLIC_STREAM_API_KEY;
-const apiSecret = process.env.STREAM_SECRET_KEY;
+const STREAM_API_KEY = process.env.NEXT_PUBLIC_STREAM_API_KEY;
+const STREAM_API_SECRET = process.env.STREAM_SECRET_KEY;
 
 export const tokenProvider = async () => {
   const user = await currentUser();
 
   if (!user) throw new Error("User is not logged in");
-  if (!apiKey) throw new Error("No API Key");
-  if (!apiSecret) throw new Error("No API Secret");
+  if (!STREAM_API_KEY) throw new Error("No API Key");
+  if (!STREAM_API_SECRET) throw new Error("No API Secret");
 
-  const client = new StreamClient(apiKey, apiSecret);
-  // exp is optional (by default the token is valid for an hour)
-  const exp = Math.round(new Date().getTime() / 1000) + 60 * 60;
-  const issued = Math.floor(Date.now() / 1000) - 60;
+  const streamClient = new StreamClient(STREAM_API_KEY, STREAM_API_SECRET);
 
-  const token = client.createToken(user.id, exp, issued);
+  const expirationTime = Math.round(new Date().getTime() / 1000) + 60 * 60;
+  const issuedAt = Math.floor(Date.now() / 1000) - 60;
+
+  const token = streamClient.createToken(user.id, expirationTime, issuedAt);
   return token;
 };
